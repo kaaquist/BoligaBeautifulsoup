3e5467891be83fa59b041edc282c866d2a97c934
Kasper Aaquist Johansen <kasperaaquist@gmail.com>
2017-04-17 18:26:24 +0200

    Add - added new xlsx feature

--- Main.py
+++ Main.py
@@ -1,16 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import requests
+import xlsxwriter
 from bs4 import BeautifulSoup
 
-"""
+
+'''
 This script was developed for a small project done on CBS.
 @author Kasper Johansen
 @co-author Jakob Stein Bügel
-"""
+'''
 
 '''This is used to avoid being stopped in Boliga firewall/haproxy etc.'''
 headers = {
     'User-agent': 'Mozilla/5.0',
 }
+
+xlsxbook = xlsxwriter.Workbook('demo.xlsx')
+xlsxsheet = xlsxbook.add_worksheet()
+
+mrd = "jan feb mar apr maj jun jul aug sep okt nov dec"
+rowcount = 0
 
 def connector(url):
     '''
@@ -28,29 +39,76 @@ def boliga_spider(max_pages):
     :return: Output print with data regarding sales figures for the property.
     '''
     page = 1
-    while page <= max_pages:
-        url = 'http://www.boliga.dk/bolig/' + str(page)
-        plain_text = connector(url).content
-        print("Title: " + (BeautifulSoup(plain_text, "html5lib").find('title')).string)
-        get_opslag(plain_text)
-        page += 1
+    try:
+        while page <= max_pages:
+            url = 'http://www.boliga.dk/bolig/' + str(page)
+            plain_text = connector(url).content
+            title = BeautifulSoup(plain_text, "html5lib").find('title').string
+            print("Title: " + title)
+            get_opslag(plain_text, title)
+            page += 1
+    except Exception as ex:
+        print (ex)
+        pass
 
 
-def get_opslag(plain_txt):
+def make_row_and_add_oldval(rowcount, columncount, oldval, prisogtype):
+    xlsxsheet.write(rowcount, columncount, oldval.decode('utf-8'))
+    columncount += 1
+    xlsxsheet.write(rowcount, columncount, prisogtype.decode('utf-8'))
+    return columncount
+
+def get_opslag(plain_txt, title):
     '''
 
     :param plain_txt:
     :return:
     '''
+    global rowcount
     soup = BeautifulSoup(plain_txt, "html5lib")
     divs = soup.find_all('div',  {'class': "row row-1 rowLine"})
+    dowrite = False
     for opslag in divs:
         print('-----------------')
+        columncount = 0
+        # This is the value that was stored in the last run below.
+        oldval = ""
         for i in [x for x in opslag.find_all('h4')]:
-            print(str(i.getText().strip()))
+            prisogtype = str(i.getText().strip().encode('utf-8'))
+            if prisogtype.split('.')[0] in mrd:
+                dowrite = True
+            if dowrite:
+                if columncount == 0:
+                    xlsxsheet.write(rowcount, columncount, title)
+                    columncount += 1
+                if "Solgt: Alm." in prisogtype:
+                    columncount = make_row_and_add_oldval(rowcount, columncount, oldval, prisogtype)
+                elif "Solgt: Ukendt" in prisogtype:
+                    columncount = make_row_and_add_oldval(rowcount, columncount, oldval, prisogtype)
+                elif "Prisændring" in prisogtype:
+                    columncount = make_row_and_add_oldval(rowcount, columncount, oldval, prisogtype)
+                elif "Prisændring" in oldval:
+                    newprisogtype = prisogtype.split("\n")
+                    for x in newprisogtype:
+                        print x
+                    xlsxsheet.write(rowcount, columncount + 2, newprisogtype[0].split(' ')[1])
+                    xlsxsheet.write(rowcount, columncount + 3, newprisogtype[0].split(' ')[0])
+                    xlsxsheet.write(rowcount, columncount + 4, newprisogtype[3].strip().split(' ')[0])
+                else:
+                    xlsxsheet.write(rowcount, columncount, prisogtype.decode('utf-8'))
+                print (prisogtype)
+                columncount += 1
+                oldval = prisogtype
         for j in [y for y in opslag.find_all('h6')]:
-            print(j.getText().strip())
+            if dowrite:
+                liggetid = str(j.getText().strip())
+                xlsxsheet.write(rowcount, columncount, liggetid.decode('utf-8'))
+                columncount += 1
+
+        if dowrite:
+            rowcount += 1
 
 
 if __name__ == '__main__':
-    boliga_spider(1)
+    boliga_spider(2)
+    xlsxbook.close()
